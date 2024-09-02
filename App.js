82f52b27e631ef842e4e368d0c45929db27b5ccc
Lanneil Linarez <124215356+LLinarez@users.noreply.github.com>
2024-09-02 12:34:09 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import react from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
+
+
+// npm cd expense-tracker-react
+
+// remember the "rafc" shortcut for new files
+// left video at 8:40 ((figure out why his income button is green and mine isnt or just ignore it, what the fuck ever))
